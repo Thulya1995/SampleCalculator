ef759a0660b0ae55d339f042e513400d5e09cf3c
Thulya Palihapitiya <thulya.16@itfac.mrt.ac.lk>
2020-11-15 03:01:21 +0530

    Changed Multiplication

--- SampleCalculator/ViewController.swift
+++ SampleCalculator/ViewController.swift
@@ -8,7 +8,38 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var numberOnScreen:Double = 0
+    
+    
+    @IBAction func buttons(_ sender: UIButton) {
+        if label.text != "" && sender.tag != 11 && sender.tag != 16{
+            if sender.tag == 12{  //Divide
+                
+            }
+            else if sender.tag == 13{  
+                
+            }
+            else if sender.tag == 14{  //Minus
+                
+            }
+            else if sender.tag == 15{  //Plus
+                
+            }
+        }
+    }
+    
+    
+    @IBOutlet weak var label: UILabel!
+    
+    
 
+    @IBAction func numbers(_ sender: UIButton) {
+        label.text = label.text! + String(sender.tag-1)
+        numberOnScreen = Double(label.text!)!
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
